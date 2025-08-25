9e459ba9672beae52ce6a8ba0a4079f1dbdb9837
manasiphand <manasiphand2005@gmail.com>
2025-08-25 12:58:28 +0530

    integrated login and signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import Authentication from "./components/Authentication";
-
-import './App.css'
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import LoginRegisterPage from "./components/LoginRegisterPage";
+import Signup from "./components/Signup";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Authentication />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginWrapper />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+// Wrapper so we can pass navigation to LoginRegisterPage
+function LoginWrapper() {
+  const navigate = useNavigate();
+  return <LoginRegisterPage onNavigateToSignup={() => navigate("/signup")} />;
+}
+
+export default App;
