9411e23f4bbc1a081a0fab735487888928540749
Diane-div <divyanshi_230178@aitpune.edu.in>
2025-08-25 14:22:04 +0530

    Student_dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import Student_dashboard from './components/Student_dashboard'
 import './App.css'
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Student_dashboard />
     </div>
   );
 }
