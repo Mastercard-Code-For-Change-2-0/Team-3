4e769740086d1fd85d98a21bf15fec82bd7d432b
Your Name <humanshu1745@gmail.com>
2025-08-25 11:52:54 +0530

    added login and signup logic

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Backend!");
-});
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
